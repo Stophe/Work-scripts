74fee43a654309857a23ff9348ac82082b98870f
Stophe <christopher.tholander@gmail.com>
2012-02-20 18:21:46 +0100

    Tidied up the code a bit

--- VASP/DataExtraction/data_extraction.py
+++ VASP/DataExtraction/data_extraction.py
@@ -6,14 +6,14 @@ from numpy import array
 
 class Outcar:
 
-    def __init__(self,path):
+    def __init__(self, path):
         self.path = path
         self.total_cpu_time = 0
         self.extract_data()
     
-    # Extracts the total CPU time in seconds
     def extract_data(self):
-        f = open("%s/OUTCAR" % (self.path),'r')
+        # Extracts the total CPU time in seconds
+        f = open("%s/OUTCAR" % (self.path), 'r')
         for line in f:
             if 'Total CPU time' in line:
                 self.total_cpu_time = line.split()[5]
@@ -21,15 +21,15 @@ class Outcar:
             
 class Oszicar:
     
-    def __init__(self,path):
+    def __init__(self, path):
         self.path = path
         self.total_energy = 0
         self.all_energies = []
         self.extract_data()
     
-    # Extracts the total energy from the last iteration    
-    def extract_data(self):    
-        f = open("%s/OSZICAR" % (self.path),'r')
+    def extract_data(self):
+        # Extracts the total energy from the last iteration        
+        f = open("%s/OSZICAR" % (self.path), 'r')
         for line in f:
             if 'E0=' in line:
                 self.all_energies.append(float(line.split()[4]))
@@ -38,18 +38,18 @@ class Oszicar:
 
 class Poscar:
     
-    def __init__(self,path):
+    def __init__(self, path):
         self.path = path
         self.title = self.extract_title()
         self.formula_unit = self.extract_formula_unit()
 
-    # Extracts the title line of the POSCAR        
     def extract_title(self):
-        return getline("%s/POSCAR" % (self.path),1)[:-1]
+        # Extracts the title line of the POSCAR        
+        return getline("%s/POSCAR" % (self.path), 1)[:-1]
 
-    # Extracts the formula unit from the POSCAR (by getting the most occurring atom type)
     def extract_formula_unit(self):
-        counts = getline("%s/POSCAR" % (self.path),7).split()
+        # Extracts the formula unit from the POSCAR (by getting the most occurring atom type)
+        counts = getline("%s/POSCAR" % (self.path), 7).split()
         maximum = counts[0]
         for count in counts:
             if count > maximum: maximum = count
@@ -57,32 +57,33 @@ class Poscar:
             
 class Kpoints:
     
-    def __init__(self,path):
+    def __init__(self, path):
         self.path = path
         self.kpoints = self.extract_kpoints()
-        self.type = self.extract_type()
-        self.total_kpoints = self.kpoints[0]*self.kpoints[1]*self.kpoints[2]
+        self.mesh_type = self.extract_type()
+        self.total_kpoints = self.kpoints[0] * self.kpoints[1] * self.kpoints[2]
         
     def __repr__(self):
-        return "%ix%ix%i" % (self.kpoints[0],self.kpoints[1],self.kpoints[2])
+        # Sets how the Kpoints class object is represented
+        return "%ix%ix%i" % (self.kpoints[0], self.kpoints[1], self.kpoints[2])
     
-    # Extracts the k-points as an array
     def extract_kpoints(self):
-        l = getline("%s/KPOINTS" % self.path, 4).split()
-        return array([int(l[0]),int(l[1]),int(l[2])])
+        # Extracts the k-points as an array
+        line = getline("%s/KPOINTS" % self.path, 4).split()
+        return array([int(line[0]), int(line[1]), int(line[2])])
     
-    # Extracts the type of the k-mesh
     def extract_type(self):
-        return getline("%s/KPOINTS" % self.path,3).split()[0]
+        # Extracts the type of the k-mesh
+        return getline("%s/KPOINTS" % self.path, 3).split()[0]
 
-# Finds the paths to the result files
-def find_data(path,list_of_paths):
+def find_data(path, list_of_paths):
+    """Finds the paths to the result files"""
     directory_list = listdir(path)
     for item in directory_list:
         if item == 'OUTCAR':
             list_of_paths.append(path)
-        elif isdir("%s/%s" % (path,item)):
-            find_data("%s/%s" % (path,item),list_of_paths)
+        elif isdir("%s/%s" % (path, item)):
+            find_data("%s/%s" % (path, item), list_of_paths)
 
 
 if __name__ == '__main__':
--- VASP/Executables/extract_this.py
+++ VASP/Executables/extract_this.py
@@ -1,66 +1,80 @@
 #!/usr/bin/env python
+from os import getcwd, system
+from os.path import isdir
+import sys
+from csv import writer,QUOTE_MINIMAL
 from data_extraction import find_data
 from data_extraction import Outcar
 from data_extraction import Oszicar
 from data_extraction import Poscar
 from data_extraction import Kpoints
-from os import getcwd
-from os import system
-from os.path import isdir
-from sys import argv
-from csv import writer
-from csv import QUOTE_MINIMAL
 
-# Checks if information from the OUTCAR is wanted
-def outcar_is_wanted():
-    if 'total_cpu_time' in argv:
+def outcar_is_needed():
+    """Checks if information from the OUTCAR is needed. Returns True if it is, no otherwise."""
+    possible_settings = ['total_cpu_time']
+    if len(set(possible_settings).intersection(set(sys.argv))) > 0:
         return True
     else:
         return False
 
-# Checks if information from the OSZICAR is wanted
-def oszicar_is_wanted():
-    if 'total_energy' or 'all_energies' in argv:
+def oszicar_is_needed():
+    """Checks if information from the OSZICAR is needed. Returns True if it is, no otherwise."""
+    possible_settings = ['total_energy', 'all_energies']
+    if len(set(possible_settings).intersection(set(sys.argv))) > 0:
         return True
     else:
         return False
 
-# Checks if information from the POSCAR is wanted
-def poscar_is_wanted():
-    if 'title' or 'formula_unit' in argv:
+def poscar_is_needed():
+    """Checks if information from the POSCAR is needed. Returns True if it is, no otherwise."""
+    possible_settings = ['title', 'formula_unit']
+    if len(set(possible_settings).intersection(set(sys.argv))) > 0:
         return True
     else:
         return False
     
-def kpoints_is_wanted():
-    possible_settings =['kpoints','kpoint_type','total_kpoints']
-    if len(set(possible_settings).intersection(set(argv))) > 0:
+def kpoints_is_needed():
+    """Checks if information from the KPOINTS is needed. Returns True if it is, no otherwise."""
+    possible_settings =['kpoints', 'kpoint_type', 'total_kpoints']
+    if set(possible_settings).intersection(set(sys.argv)):
         return True
     else:
         return False
 
-# Extracts the data specified in the input arguments
-def main(current_path):
-    result_csv_file = writer(open('%s/results.csv' % current_path,'wb'),
-                             delimiter=',',quotechar='|',quoting=QUOTE_MINIMAL)
-    if 'all_energies' in argv:
-        energy_csv_file = writer(open('%s/all_energies.csv' % current_path,'wb'),
-                                 delimiter=',',quotechar='|',quoting=QUOTE_MINIMAL)
-    data_paths =[] 
+def main():
+    """
+    The main function. Extracts the data specified in the input arguments.
+    
+    If no argument is given the default is to get all data.
+    If a path is given, that will be used as the current directory.
+    """
+    current_path = getcwd()
+    if len(sys.argv) == 1:
+        sys.argv = sys.argv + ['kpoints', 'kpoint_type', 'total_kpoints', 'title', 'formula_unit',
+                               'total_energy', 'all_energies','total_cpu_time', 'print']
+    elif isdir(sys.argv[1]):
+        current_path = sys.argv[1]
+    rf = open('%s/results.csv' % current_path, 'wb')
+    result_csv_file = writer(rf, delimiter=',', quotechar='|', quoting = QUOTE_MINIMAL)
+    if 'all_energies' in sys.argv:
+        ef = open('%s/all_energies.csv' % current_path, 'wb')
+        energy_csv_file = writer(ef, delimiter=',', quotechar='|', quoting = QUOTE_MINIMAL)
+    data_paths = [] 
     find_data(current_path,data_paths)
     for path in data_paths:
-        if outcar_is_wanted(): outcar = Outcar(path)
-        if oszicar_is_wanted(): oszicar = Oszicar(path)
-        if poscar_is_wanted(): poscar = Poscar(path)
-        if kpoints_is_wanted(): kpoints = Kpoints(path)
-        results=[]
-        for argument in argv:
+        if outcar_is_needed(): outcar = Outcar(path)
+        if oszicar_is_needed(): oszicar = Oszicar(path)
+        if poscar_is_needed(): poscar = Poscar(path)
+        if kpoints_is_needed(): kpoints = Kpoints(path)
+        results = []
+        
+        for argument in sys.argv:
             if argument == 'title':
                 results.append(poscar.title)
             elif argument == 'total_energy':
                 results.append(oszicar.total_energy)
             elif argument == 'all_energies':
-                energy_csv_file.writerow([poscar.title]+oszicar.all_energies)
+                energy_csv_file.writerow([poscar.title] + oszicar.all_energies)
             elif argument == 'formula_unit':
                 results.append(poscar.formula_unit)
             elif argument == 'total_cpu_time':
@@ -70,15 +84,15 @@ def main(current_path):
             elif argument == 'total_kpoints':
                 results.append(kpoints.total_kpoints)
             elif argument == 'kpoint_type':
-                results.append(kpoints.type)
+                results.append(kpoints.mesh_type)
         result_csv_file.writerow(results)
+    rf.close()
+    ef.close()
+    
+    if 'print' in sys.argv: 
+        print "AAAAAAA"
+        from os import path as os_path
+        system('/bin/cat "%s"' % os_path.join(current_path, "results.csv"))
 
 if __name__ == '__main__':
-    current_path = getcwd()
-    if len(argv) == 1:
-        argv = argv + ['title','total_energy','formula_unit','total_cpu_time','all_energies',
-                       'kpoints','total_kpoints','kpoint_type']
-    elif isdir(argv[1]):
-        current_path = argv[1]
-    main(current_path)
-    if 'print' in argv: system("cat '%s/results.csv'" % current_path)
+    main()
