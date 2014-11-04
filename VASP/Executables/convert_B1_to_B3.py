a7a95e09ac2997d7f5ee43bfbdc1cc2f890e07f6
Christopher Tholander <christopher.tholander@gmail.com>
2014-11-04 17:24:54 +0100

    Added old stuff to convert script again.

--- VASP/Executables/convert_B1_to_B3.py
+++ VASP/Executables/convert_B1_to_B3.py
@@ -15,12 +15,16 @@ def convert_structure():
     supercell_x = int(raw_input("Supercells in x-direction: "))
     supercell_y = int(raw_input("Supercells in y-direction: "))
     supercell_z = int(raw_input("Supercells in z-direction: "))
+    direction = raw_input("111 direction?: ")
     
     for atom in sc.atoms:
         if atom.symbol == 'N':
-            atom.position[0] = atom.position[0] - 0.25/supercell_x
-            atom.position[1] = atom.position[1] - 0.25/supercell_y
-            atom.position[2] = atom.position[2] - 0.25/supercell_z
+            if direction in ("Y", "y", "yes", "Yes", "YES"):
+                atom.position[2] = atom.position[2] - 0.25/supercell_z
+            else:
+                atom.position[0] = atom.position[0] - 0.25/supercell_x
+                atom.position[1] = atom.position[1] - 0.25/supercell_y
+                atom.position[2] = atom.position[2] - 0.25/supercell_z
     
     poscar.create_file()
 
