82ff24c74b7afaa596afceeeea6b324ebab8b18e
Christopher Tholander <christopher.tholander@gmail.com>
2014-12-06 16:23:23 +0100

    Patch calculate_sqs method

--- VASP/vasp/contcar.py
+++ VASP/vasp/contcar.py
@@ -167,6 +167,19 @@ class Contcar(object):
         return delta
     
     def _calculate_sqs_repetitions(self, other):
+        possible_structures = [(4, 2, 4),(4, 4, 2)] 
+        
+        for structure in possible_structures:
+            atoms = 0
+            
+            for atoms in self.supercell.atoms:
+                if (atom.position[0] <= 1.0 / structure[0] and 
+                    atom.position[1] <= 1.0 / structure[1] and 
+                    atom.position[2] <= 1.0 / structure[2]):
+                    atoms += 1
+            if atoms == 4: return structure
+        return (0, 0, 0)
+        
         scx = 0
         scy = 0
         scz = 0
