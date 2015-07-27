42c36d26ef9cc3fbb70713fb9efa06ffc083e849
Christopher Tholander <christopher.tholander@gmail.com>
2015-07-27 10:45:36 +0200

    Adds extraction of permittivity

--- VASP/vasp/outcar.py
+++ VASP/vasp/outcar.py
@@ -27,6 +27,7 @@ class Outcar(object):
         self.kpoints = (0, 0, 0)
         self.nkpts = 0
         self.polarization = [[],[]]
+        self.permittivity = []
         self.born_charge_33_average = 0
         self.ci_e33 = 0
         self._extract_data()
@@ -82,6 +83,14 @@ class Outcar(object):
                     bec.append(l[3])
                     f.next()
                 self.born_charge_33_average = sum(abs(bec))/len(bec)
+            elif 'MACROSCOPIC STATIC DIELECTRIC TENSOR (including local field effects in DFT)' in line:
+                f.next()
+                l = f.next().split()
+                self.permittivity.append(l)
+                l = f.next().split()
+                self.permittivity.append(l)
+                l = f.next().split()
+                self.permittivity.append(l)
             elif 'volume of cell' in line:
                 self.volume = float(line.split()[4])
             elif 'Total CPU time' in line:
